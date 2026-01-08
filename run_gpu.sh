e666893efea27a6dd3f11e185c8f708a0f295e7e
Samuel <spr2lepinguin@gmail.com>
2026-01-08 23:16:17 +0100

    saal

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,4 +1,4 @@
 module load conda
-conda activate XXX(your environement to put)
+conda activate pytorche(your environement to put)
 torchrun --nnodes=1 --nproc-per-node=1 demo_gpu.py
 torchrun --nnodes=1 --nproc-per-node=2 demo_gpu.py
